9113b78db0166d2075b7441515d3252b20112553
Ezequiel Cicala <ezequielcicala@duck.com>
2025-10-24 21:29:40 -0300

    refactor: update dependencies and configuration for Vue 3 integration
    - Added Vue 3 and related packages to package.json and package-lock.json.
    - Integrated @wxt-dev/module-vue into wxt.config.ts for module support.

--- wxt.config.ts
+++ wxt.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'wxt';
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
+  modules: ['@wxt-dev/module-vue'],
   imports: {
     eslintrc: {
       enabled: 9,
