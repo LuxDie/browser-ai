1225620fd3b4a271f6d61d5586b68a7a278fdde1
Ezequiel Cicala <ezequielcicala@duck.com>
2025-11-20 22:31:06 -0300

    refactor(sidepanel): extract DetectedLanguage component

---
+++ src/components/DetectedLanguage.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { LanguageService, type SupportedLanguageCode } from '@/entrypoints/background/language/language.service';
+
+const languageService = LanguageService.getInstance();
+
+defineProps<{
+  sourceLanguage: SupportedLanguageCode | null;
+}>();
+</script>
+
+<template>
+  <div v-if="sourceLanguage">
+    {{ browser.i18n.getMessage('detectedLanguage', [languageService.getLanguageKey(sourceLanguage)]) }}
+  </div>
+</template>
