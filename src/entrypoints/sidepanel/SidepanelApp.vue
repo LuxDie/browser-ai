ed80d4a0f49d9da837669d14e5ae91918adc7f82
Ezequiel Cicala <ezequielcicala@duck.com>
2025-11-20 21:36:19 -0300

    refactor(sidepanel): extract OutputArea component

--- src/entrypoints/sidepanel/SidepanelApp.vue
+++ src/entrypoints/sidepanel/SidepanelApp.vue
@@ -176,14 +176,6 @@ watch(summarize, () => {
       {{ error }}
     </div>
 
-    <div v-if="translatedText" class="flex flex-col gap-2">
-      <div class="flex justify-between items-center">
-        <label for="output-text">Result:</label>
-        <span id="processing-source" class="text-xs bg-green-100 text-green-800 px-2 py-1 rounded-full">
-          {{ t('localProcessingBadge') }}
-        </span>
-      </div>
-      <textarea id="output-text" :value="translatedText" class="border p-2 rounded-md" rows="5" readonly></textarea>
-    </div>
+    <OutputArea :translated-text="translatedText" />
   </div>
 </template>
